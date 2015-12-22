affe2d977a5d521dfe20c26f8ca4cd0a34d112de
JTL4383 <joshua_t_lunsford@homedepot.com>
2015-12-21 20:03:23 -0500

    Updated to ironworks v1.0

--- src/typings/master.d.ts
+++ src/typings/master.d.ts
@@ -1,6 +1,5 @@
 
-///<reference path='./tsd/tsd.d.ts' />
 
 ///<reference path='./redis/redis.d.ts' />
 
-///<reference path='../../node_modules/ironworks/ironworks.d.ts' />
+///<reference path='../../node_modules/ironworks/dist/ironworks.d.ts' />
